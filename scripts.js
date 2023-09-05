ce2de9179227df2154e4fc0d5864ba685ef51394
Rubén Valls <r.vallsaparici@gmail.com>
2023-09-05 12:27:16 +0200

    added background image and some styles, working on arrow functions

--- scripts.js
+++ scripts.js
@@ -1,13 +1,17 @@
 let images = [];
 let query = '';
+let position = 0;
 const KEY = 'iVlHyj-ZdiU_jqN6P4ksYTzs75U_09Kpz84VmSkA2bI';
 const API = `https://api.unsplash.com/photos/random?client_id=${KEY}&count=${10}&query=${query}`;
+const prevImage = document.querySelector('#prev');
+const nextImage = document.querySelector('#next');
 
 const getImages = () => {
     fetch(API)
         .then(response => response.json())
         .then(data => {
             images = [...data];
+            position = 0;
             printImage()
         })
         .catch(error => console.log(error));
@@ -16,6 +20,7 @@ const getImages = () => {
 const printImage = (position = 0) => {
     const imageContainer = document.querySelector('.image-container');
     imageContainer.innerHTML = `<img src=${images[position].urls.full} alt=${images[position].alt_description}>`
+    document.body.style.backgroundImage = `url(${images[position].urls.full})`;
 }
 
 getImages();
