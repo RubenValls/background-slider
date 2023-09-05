3dce87ef6867c144b302634f6ada8626f2bc7bbc
Rubén Valls <r.vallsaparici@gmail.com>
2023-09-05 14:08:27 +0200

    finder working correctly, project finished

--- scripts.js
+++ scripts.js
@@ -2,16 +2,17 @@ let images = [];
 let query = '';
 let position = 0;
 const KEY = 'iVlHyj-ZdiU_jqN6P4ksYTzs75U_09Kpz84VmSkA2bI';
-const API = `https://api.unsplash.com/photos/random?client_id=${KEY}&count=${10}&query=${query}`;
 const prevImage = document.querySelector('#prev');
 const nextImage = document.querySelector('#next');
+const finder = document.querySelector('#query');
+const iconFinder = document.querySelector('#icon-finder');
 
 const getImages = () => {
+    const API = `https://api.unsplash.com/photos/random?client_id=${KEY}&count=${10}&query=${query}`;
     fetch(API)
         .then(response => response.json())
         .then(data => {
             images = [...data];
-            console.log(images)
             position = 0;
             printImage()
         })
@@ -44,4 +45,18 @@ nextImage.addEventListener('click', () => {
         : position += 1;
     
     printImage(position);
+})
+
+finder.addEventListener('keydown', (event) => {
+    if(event.keyCode === 13){
+        query = finder.value;
+        finder.value = '';
+        getImages();
+    }
+})
+
+iconFinder.addEventListener('click', () => {
+    query = finder.value;
+    finder.value = '';
+    getImages();
 })
